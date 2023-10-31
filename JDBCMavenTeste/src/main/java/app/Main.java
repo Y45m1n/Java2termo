e06f1f432de98c0528d53f76db663fcddb3aea97
Y45m1n <yasmindeoliveira286@gmail.com>
2023-10-31 16:08:10 -0300

    oi

--- JDBCMavenTeste/src/main/java/app/Main.java
+++ JDBCMavenTeste/src/main/java/app/Main.java
@@ -10,10 +10,11 @@ public class Main {
 Statement s; // Cria uma instância de Statement para executar comandos SQL.
 String usuario = "postgres"; // Nome de usuário do banco de dados.
 String senha = "postgres"; // Senha do banco de dados.
+
 String url = "jdbc:postgresql://localhost:5432/postgres"; // URL de conexão com o banco de dados PostgreSQL.
 
 // Estabelece a conexão com o banco de dados.
-Connection c = DriverManager.getConnection(url, usuario, senha);
+Connection c = DriverManager.getConnection(url, usuario, senha); //1°etapa driverManager 2°connection = estabelece um objeto que esta conectado ao banco 3° close = fechar conexao com banco
 s = c.createStatement(); // Cria um objeto Statement a partir da conexão.
 // Executa uma codigo SQL de inserção na tabela "contato".
 // ser substituídos pelos valores reais a serem inseridos.
